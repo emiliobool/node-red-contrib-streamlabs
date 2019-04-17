442fa22c3f949d417c6d21e572764e5fe2cd60d0
Emilio Bool <contact@emiliobool.com>
2019-04-16 22:48:44 -0700

    fix: connection bug

--- src/config.ts
+++ src/config.ts
@@ -7,12 +7,21 @@ export interface ConfigNode extends Node{
     socket_token: string;
     getSocket(): SocketIOClient.Socket;
     socket: SocketIOClient.Socket | null;
+    streamlabels: any;
+    streamlabelsUnderlying: any;
 }
 export interface Config extends NodeProperties{
     name: string;
     access_token: string;
     socket_token: string;
 }
+export interface StreamlabsEvent{
+    type: string;
+    message: any;
+    event_id: string;
+    for?: string;
+}
+
 module.exports = function(RED: Red) {
   function TwitchJsClient(
     this: ConfigNode,
@@ -22,13 +31,28 @@ module.exports = function(RED: Red) {
     this.name = config.name
     this.access_token = config.access_token
     this.socket_token = config.socket_token
+    this.streamlabels = {}
+    this.streamlabelsUnderlying = {}
     this.getSocket = () => {
         if(!this.socket){
             this.socket = io(`https://sockets.streamlabs.com/?token=${this.socket_token}`)
-            this.socket.on("event", (event: object) => this.log(event))
+            this.socket.on("event", (event: StreamlabsEvent) => {
+                if(event.type === "streamlabels"){
+                    this.streamlabels = event.message
+                    this.context().global.set(`${this.name}_streamlabels`, event.message)
+                }else if(event.type === "streamlabels.underlying")
+                    this.streamlabelsUnderlying = event.message
+                    this.context().global.set(`${this.name}_streamlabels.underlying`, event.message)
+            })
         }
         return this.socket
     }
+    this.on('close', (done) => {
+        if(this.socket){
+            this.socket.close()
+        }
+        done()
+    })
   }
   RED.nodes.registerType("streamlabs-config", TwitchJsClient);
 };
--- src/socket.ts
+++ src/socket.ts
@@ -1,20 +1,32 @@
-import { Red, NodeProperties, Node } from "node-red";
-import { ConfigNode } from "./config";
+import { Red, NodeProperties, Node } from "node-red"
+import { ConfigNode } from "./config"
+import { inspect } from 'util'
 
 export interface SocketConfig extends NodeProperties {
-  config: string;
-  type_filter: string;
+    config: string
+    type_filter: string
 }
 
-module.exports = function(RED: Red) {
-  function Socket(this: Node, config: SocketConfig) {
-    RED.nodes.createNode(this, config)
-    const configNode = RED.nodes.getNode(config.config) as ConfigNode
-    configNode.getSocket().on("event", (payload: any) => {
-      if(!config.type_filter || payload.type === config.type_filter){
-        this.send({ payload });
-      }
-    })
-  }
-  RED.nodes.registerType("streamlabs-socket", Socket);
+module.exports = function (RED: Red) {
+    function Socket(this: Node, config: SocketConfig) {
+        RED.nodes.createNode(this, config)
+        const configNode = RED.nodes.getNode(config.config) as ConfigNode
+        configNode.getSocket().on("event", (payload: any) => {
+            
+            if (!config.type_filter || payload.type === config.type_filter) {
+                try{
+                    if(payload.type === 'streamlabels'){
+                        delete payload.message.underlying
+                    }
+                    payload = JSON.parse(JSON.stringify(payload))
+                    this.send({ payload })
+                }catch(error){
+                    this.error(error.toString())
+                    this.error(inspect(payload))
+                }
+
+            }
+        })
+    }
+    RED.nodes.registerType("streamlabs-socket", Socket)
 };
